1b82ae8db574b2009a8332ae1a97692dd0f3ee43
paulo-guerin <paularguerin@gmail.com>
2018-11-05 18:02:23 +0100

    Premiers tests Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
-  
+    before_action :require_login
+
+    def require_login
+
+        unless logged_in?
+    
+          flash[:danger] = "Tu dois être enregistré pour accéder à cette page, petit malin ;)"
+    
+          redirect_to login_path # halts request cycle
+    
+        end
+    
+    end
 end
